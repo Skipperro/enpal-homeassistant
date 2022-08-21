7efaa3d499c09218cb87c2ec5f8e773a4c360e84
Tomasz <skipperro@gmail.com>
2022-08-21 16:22:24 +0200

    Added battery estimate

--- custom_components/enpal/sensor.py
+++ custom_components/enpal/sensor.py
@@ -52,14 +52,19 @@ async def async_setup_entry(
 
     tables = await hass.async_add_executor_job(get_tables, config['enpal_host_ip'], config['enpal_host_port'], config['enpal_token'])
 
+    production_found = False
+    consumption_found = False
+
     for table in tables:
         field = table.records[0].values['_field']
         measurement = table.records[0].values['_measurement']
 
         if measurement == "Gesamtleistung" and field == "Produktion":
             to_add.append(EnpalSensor(field, measurement, 'mdi:solar-power', 'Solar Production', config['enpal_host_ip'], config['enpal_host_port'], config['enpal_token'], 'power', 'W'))
+            production_found = True
         if measurement == "Gesamtleistung" and field == "Verbrauch":
             to_add.append(EnpalSensor(field, measurement, 'mdi:lightning-bolt', 'Power Consumption', config['enpal_host_ip'], config['enpal_host_port'], config['enpal_token'], 'power', 'W'))
+            consumption_found = True
         if measurement == "inverterTemperature" and field == "Temperature":
             to_add.append(EnpalSensor(field, measurement, 'mdi:thermometer', 'Inverter Temperature', config['enpal_host_ip'], config['enpal_host_port'], config['enpal_token'], 'temperature', '°C'))
         if measurement == "gridFrequency" and field == "Frequenz":
@@ -82,6 +87,9 @@ async def async_setup_entry(
         if measurement == "productionVoltageDc" and field == "String2":
             to_add.append(EnpalSensor(field, measurement, 'mdi:solar-power', 'DC Voltage String 2', config['enpal_host_ip'], config['enpal_host_port'], config['enpal_token'], 'voltage', 'V'))
 
+    if production_found and consumption_found:
+        to_add.append(BatteryEstimate(10.0))
+
     entity_registry = async_get(hass)
     entries = async_entries_for_config_entry(
         entity_registry, config_entry.entry_id
@@ -91,8 +99,62 @@ async def async_setup_entry(
 
     async_add_entities(to_add, update_before_add=True)
 
+class BatteryEstimate(SensorEntity):
+    def __init__(self, max_capacity: float):
+        self.max_capacity = max_capacity
+        self._attr_native_value = max_capacity
+
+        self._attr_icon = 'mdi:home-battery'
+        self._attr_name = 'Battery Capacity Estimate'
+        self._attr_unique_id = str(uuid.uuid4())
+        self._attr_extra_state_attributes = {}
+        self._attr_extra_state_attributes['last_check'] = datetime.now()
+
+        self._attr_device_info = DeviceInfo(
+            entry_type=DeviceEntryType.SERVICE,
+            identifiers={(DOMAIN, 'enpal')},
+            name="Enpal Solar Installation",
+        )
+
+    async def async_update(self) -> None:
+        try:
+            self._attr_device_class = 'energy'
+            self._attr_native_unit_of_measurement = 'kWh'
+            self._attr_state_class = 'measurement'
+
+            # get last_check of this sensor from extra state attributes
+            last_check = self.hass.states.get(self.entity_id).attributes['last_check']
+            # get value of sensor.power_consumption from hass
+            power_consumption = self.hass.states.get('sensor.power_consumption').state
+            # get value of sensor.power_production from hass
+            power_production = self.hass.states.get('sensor.solar_production').state
+            # calculate battery estimate
+            battery_change = float(power_production) - float(power_consumption)
+
+            # calculate battery change in kWh for the time between last check and now
+            battery_change_kwh = battery_change * (datetime.now() - last_check).seconds / 3600
+
+            # get current battery capacity from hass
+            battery_capacity = self.hass.states.get(self.entity_id).state
+            if battery_capacity == None:
+                battery_capacity = self.max_capacity
+
+            # Set new battery capacity
+            end_value = battery_capacity + battery_change_kwh
+            if end_value > self.max_capacity:
+                end_value = self.max_capacity
+            if end_value < 0:
+                end_value = 0
+            self._attr_native_value = end_value
+            self._attr_extra_state_attributes['last_check'] = datetime.now()
+
+        except Exception as e:
+            _LOGGER.error(f'{e}')
+            self._state = 'Error'
+            self._attr_native_value = None
+            self._attr_extra_state_attributes['last_check'] = datetime.now()
+
 class EnpalSensor(SensorEntity):
-    """Representation of a Sensor."""
 
     def __init__(self, field: str, measurement: str, icon:str, name: str, ip: str, port: int, token: str, device_class: str, unit: str):
         self.field = field
