14d2fbe4dd9a14f4bbc7c2e0dfc97022c8236130
Tomasz <skipperro@gmail.com>
2024-11-27 16:12:13 +0100

    Migrate to async_forward_entry_setups for compatibility with HA 2025

--- custom_components/enpal/__init__.py
+++ custom_components/enpal/__init__.py
@@ -23,7 +23,7 @@ async def async_setup_entry(
 
     # Forward the setup to the sensor platform.
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
